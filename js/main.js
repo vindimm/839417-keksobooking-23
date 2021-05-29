b063d364a9cc06031a60752255b352c668821aea
Dimahtml <bomabo@mail.ru>
2021-05-29 16:31:19 +0500

    refactoring. arrow functions. undefined -> null

--- js/main.js
+++ js/main.js
@@ -1,23 +1,22 @@
-function getPositiveRandomIntInclusive (min, max) {
-  if (max < min || min < 0 || max < 0) {
-    return undefined;
+const getPositiveRandomIntInclusive = (min, max) => {
+  if (max < min || min < 0) {
+    return null;
   }
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
-function getPositiveRandomFloatInclusive (min, max, fractionLength) {
-  if (fractionLength < 0) {
-    return undefined;
+const getPositiveRandomFloatInclusive = (min, max, fractionLength) => {
+  if (max < min || min < 0 || fractionLength < 0) {
+    return null;
   }
   fractionLength = Math.floor(fractionLength);
   min = min * Math.pow(10, fractionLength);
   max = max * Math.pow(10, fractionLength);
-  let result = getPositiveRandomIntInclusive(min, max);
-  result = result / Math.pow(10, fractionLength);
-  return result;
-}
+  const result = getPositiveRandomIntInclusive(min, max);
+  return result / Math.pow(10, fractionLength);
+};
 
 getPositiveRandomIntInclusive();
 getPositiveRandomFloatInclusive();
